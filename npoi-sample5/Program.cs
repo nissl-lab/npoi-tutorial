5a2c9eabd517aeb9dad50a5ec0a68ea099285d1b
Tony Qu <tonyqus@gmail.com>
2024-11-02 22:42:48 +0800

    add npoi-sample5

---
+++ npoi-sample5/Program.cs
@@ -0,0 +1,21 @@
+﻿using NPOI.SS.UserModel;
+using System;
+using System.IO;
+
+
+namespace WorkbookFactoryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream= File.OpenRead("fakeExcel.xls"))
+            {
+                IWorkbook workbook = WorkbookFactory.Create(stream);
+
+                Console.WriteLine("workbook type: " + workbook.GetType().Name);
+                Console.ReadLine();
+            }
+        }
+    }
+}
